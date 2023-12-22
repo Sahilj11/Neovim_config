8e4dc0374f8ccc8745427971db95f5243ea27b41
Sahil jalandhara <sahiljalandhara123@gmail.com>
2023-12-22 07:51:44 +0530

    updated somethings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,37 +1,37 @@
-local lazy = {}
-
-function lazy.install(path)
-	if not vim.loop.fs_stat(path) then
-		print("Installing lazy.nvim....")
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"https://github.com/folke/lazy.nvim.git",
-			"--branch=stable", -- latest stable release
-			path,
-		})
-	end
+-- function lazy.install(path)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+-- end
 
-function lazy.setup(plugins)
-	if vim.g.plugins_ready then
-		return
-	end
+-- function lazy.setup(plugins)
+-- 	if vim.g.plugins_ready then
+-- 		return
+-- 	end
+--
+-- 	-- You can "comment out" the line below after lazy.nvim is installed
+-- 	-- lazy.install(lazy.path)
+--
+-- 	vim.opt.rtp:prepend(lazy.path)
+--
+-- 	require("lazy").setup(plugins, lazy.opts)
+-- 	vim.g.plugins_ready = true
+-- end
+vim.opt.rtp:prepend(lazypath)
 
-	-- You can "comment out" the line below after lazy.nvim is installed
-	-- lazy.install(lazy.path)
 
-	vim.opt.rtp:prepend(lazy.path)
+--lazy.path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--lazy.opts = {}
 
-	require("lazy").setup(plugins, lazy.opts)
-	vim.g.plugins_ready = true
-end
-
-lazy.path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-lazy.opts = {}
-
-lazy.setup({
+require("lazy").setup({
 	-- List of plugins
 	{ "nvim-treesitter/nvim-treesitter" },
 	{
